22f3ee383b0b559acf842fedea56d65cbbedefeb
Elisarmor <ejsisgreat@gmail.com>
2019-03-20 09:08:18 -0500

    Create not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HomeModule } from './apps/home/home.module';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app-component/app.component';
 import { JiknoAppComponent } from './app-component/jikno-app/jikno-app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JiknoAppComponent
+    JiknoAppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
