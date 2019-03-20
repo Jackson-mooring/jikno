b2317f4b1c15b34d390a1ef1084af430c8f7bf32
Elisarmor <ejsisgreat@gmail.com>
2019-03-20 09:57:30 -0500

    Add a comment for clairity

--- src/app/routing/app-routes.ts
+++ src/app/routing/app-routes.ts
@@ -1,3 +1,8 @@
+/*
+* If you have a route that you want to serve through jikno-app component, place it in here.
+* Only put the first part of the route in this array. (e.g. '/some-route' not '/some-route/some-child' )
+* Remember to always put a slash in front of the route. (e.g. '/some-route' not 'some-route' )
+*/
 export const AppRoutes = [
     '/home',
 ]
