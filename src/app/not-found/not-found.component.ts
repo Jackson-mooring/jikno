1b338791833433f7a036d58043a564bfa091a7b7
Jackson <jackson.mooring@gmail.com>
2019-03-20 17:47:35 -0500

    Create not-found page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.sass']
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found.component.sass']
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        typeWriter();
+    }
+}
+        let i = 0;
+function typeWriter() {
 
+        const txt = 'The Page You are Looking for Cannot be Found On This Side Of The Plannet.';
+        if (i < txt.length) {
+        const speed = 50;
+        document.getElementById('content').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
 }
