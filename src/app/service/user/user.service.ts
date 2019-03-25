66acf319283dbbccf9452d99fbb0da70282dc1ea
Elisarmor <ejsisgreat@gmail.com>
2019-03-25 11:22:48 -0500

    Add content to service

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
 
+import { UserVals } from '../../model/user-vals';
+
 @Injectable({
 	providedIn: 'root'
 })
 export class UserService {
 
 	constructor() { }
+
+	public getUser(): UserVals {
+		const email = localStorage.getItem('email');
+		const password = localStorage.getItem('password');
+		return {
+			email,
+			password,
+		}
+	}
+
+	public setUser(email: string, password: string) {
+		localStorage.setItem('email', email);
+		localStorage.setItem('password', password);
+	}
+
+	public cleanUser(complete?: boolean) {
+		if (complete === true) localStorage.clear();
+		else {
+			localStorage.removeItem('email');
+			localStorage.removeItem('password');
+		}
+	}
+
+	public isUser(): boolean {
+		const item1 = localStorage.getItem('email');
+		const item2 = localStorage.getItem('password');
+		if (item1 == null || item2 == null) return false
+		else return true;
+	}
 }
