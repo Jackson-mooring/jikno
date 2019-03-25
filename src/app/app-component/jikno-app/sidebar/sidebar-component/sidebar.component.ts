7b5d91c9801626b44d98da6cf327be1b98ee9c1a
Elisarmor <ejsisgreat@gmail.com>
2019-03-25 07:31:17 -0500

    Finish up

--- src/app/app-component/jikno-app/sidebar/sidebar-component/sidebar.component.ts
+++ src/app/app-component/jikno-app/sidebar/sidebar-component/sidebar.component.ts
@@ -1,33 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Router, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { DataService } from '../../../../service/data/data.service';
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.sass'],
-  animations: [
-    trigger('showHideBar', [
-      state('in', style({
-        left: '0',
-      })),
-      state('out', style({
-        left: '-300px',
-      })),
-      transition('* => *', [
-        animate('200ms ease-in-out'),
-      ])
-    ])
-  ],
+	selector: 'app-sidebar',
+	templateUrl: './sidebar.component.html',
+	styleUrls: ['./sidebar.component.sass'],
+	animations: [
+		trigger('showHideBar', [
+			state('in', style({
+				left: '0',
+			})),
+			state('out', style({
+				left: '-300px',
+			})),
+			transition('* => *', [
+				animate('200ms ease-in-out'),
+			])
+		])
+	],
 })
 export class SidebarComponent implements OnInit {
-  constructor(
-    public dataService: DataService,
-  ) { }
+	constructor(
+		public dataService: DataService,
+		private router: Router,
+		private routeLocation: Location,
+	) { }
 
-  ngOnInit() {
-  }
+	route: string = '';
+
+	ngOnInit() {
+		this.createRouteActions();
+		this.router.events.forEach((event) => {
+			if (event instanceof NavigationEnd) {
+				this.createRouteActions();
+			}
+		});
+	}
+
+	createRouteActions() {
+		this.route = this.routeLocation.path()
+	}
 
 }
