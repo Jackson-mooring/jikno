1d13a4d7704c7593db1292640474266c2916c2e7
Elisarmor <ejsisgreat@gmail.com>
2019-03-26 17:19:44 -0500

    Fiddle with selector

--- src/app/app-component/jikno-app/apps-overlay/apps-overlay.component.ts
+++ src/app/app-component/jikno-app/apps-overlay/apps-overlay.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-apps-overlay',
+	selector: 'apps-overlay',
 	templateUrl: './apps-overlay.component.html',
 	styleUrls: ['./apps-overlay.component.sass']
 })
