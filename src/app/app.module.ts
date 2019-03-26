60004ea44a32071af8e58238c557a4a2bb5ab9b6
Elisarmor <ejsisgreat@gmail.com>
2019-03-26 17:19:49 -0500

    Fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app-component/app.component';
 import { JiknoAppComponent } from './app-component/jikno-app/jikno-app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { AppsOverlayComponent } from './apps-overlay/apps-overlay.component';
+import { AppsOverlayComponent } from './app-component/jikno-app/apps-overlay/apps-overlay.component';
 
 @NgModule({
 	declarations: [
