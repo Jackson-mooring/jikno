938c21ed7c45c8ec4edea33767e1e8cfe20b0daa
Elisarmor <ejsisgreat@gmail.com>
2019-03-27 19:46:02 -0500

    Fix errors in not-found

--- src/app/apps/home/home.module.ts
+++ src/app/apps/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CoreModule } from '../../shared/core.module';
+import { AppRoutingModule } from '../../routing/app-routing.module';
 
 import { HomeComponent } from './home/home.component';
 
@@ -9,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 	],
 	imports: [
 		CoreModule,
+		AppRoutingModule,
 	]
 })
 export class HomeModule { }
