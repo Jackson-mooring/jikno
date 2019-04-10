4304be3f54a326d1c58b01fdcd6f5c258beb4db6
Elisarmor <ejsisgreat@gmail.com>
2019-04-10 08:51:01 -0500

    Change the "new-password" route

--- src/app/forgot-password/routes/routes.ts
+++ src/app/forgot-password/routes/routes.ts
@@ -6,6 +6,6 @@ export const ForgotPasswordRoutes: Routes = [
 	{ path: 'forgot-password', redirectTo: 'forgot-password/enter-email', pathMatch: 'full' },
 	{ path: 'forgot-password/enter-email', component: MainComponent },
 	{ path: 'forgot-password/enter-code', component: MainComponent },
-	{ path: 'forgot-password/new-password', component: MainComponent },
+	{ path: 'new-password', component: MainComponent },
 	{ path: 'forgot-password/continue', component: MainComponent },
 ]
