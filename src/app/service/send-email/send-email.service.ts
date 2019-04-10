e989d9aa3ad16c24b0e52b9270ea0f2971148fa8
Elisarmor <ejsisgreat@gmail.com>
2019-04-10 11:29:57 -0500

    Add integration for no internet

--- src/app/service/send-email/send-email.service.ts
+++ src/app/service/send-email/send-email.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map, retry, catchError, timeout } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { JIKNO_API_ROOT, POST_HEADERS, JIKNO_API_KEY } from '../../constants/constants';
@@ -18,10 +18,16 @@ export class SendEmailService {
 		const params = "?action=email_user&key=" + JIKNO_API_KEY;
 		return this.http.post(JIKNO_API_ROOT + params, body, POST_HEADERS)
 		.pipe(
+			map(res => {
+				console.log(res);
+				return "EMAILED";
+			}),
 			retry(3),
-			catchError(err => "Could not connect to email"),
-			map(res => "EMAILED"),
-			timeout(5000)
+			timeout(5000),
+			catchError(err => {
+				console.error(err);
+				return of("Could not connect to email");
+			})
 		)
 	}
 }
