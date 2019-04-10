ec6dc18be0690390b2541f20c5661bc8e8758cf0
Elisarmor <ejsisgreat@gmail.com>
2019-04-09 20:35:59 -0500

    Get the main component going

--- src/app/forgot-password/main/main.component.ts
+++ src/app/forgot-password/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
 	selector: 'app-main',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-	constructor() { }
+	constructor(
+		public routeLocation: Location,
+	) { }
 
 	ngOnInit() {
 	}
--- src/app/forgot-password/routes/routes.ts
+++ src/app/forgot-password/routes/routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 
-import { EnterEmailComponent } from '../enter-email/enter-email.component';
-import { EnterCodeComponent } from '../enter-code/enter-code.component';
-import { NewPasswordComponent } from '../new-password/new-password.component';
+import { MainComponent } from '../main/main.component';
 
 export const ForgotPasswordRoutes: Routes = [
-	{ path: '/forgot-password', redirectTo: '/forgot-password/enter-email', pathMatch: 'full' },
-	{ path: '/forgot-password/enter-email', component: EnterEmailComponent },
-	{ path: '/forgot-password/enter-code', component: EnterCodeComponent },
-	{ path: '/forgot-password/new-password', component: NewPasswordComponent },
+	{ path: 'forgot-password', redirectTo: 'forgot-password/enter-email', pathMatch: 'full' },
+	{ path: 'forgot-password/enter-email', component: MainComponent },
+	{ path: 'forgot-password/enter-code', component: MainComponent },
+	{ path: 'forgot-password/new-password', component: MainComponent },
+	{ path: 'forgot-password/continue', component: MainComponent },
 ]
