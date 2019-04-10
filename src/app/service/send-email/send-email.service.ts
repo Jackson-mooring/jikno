be18f0f500132838f0563d46eaf4818888548ef7
Elisarmor <ejsisgreat@gmail.com>
2019-04-10 12:52:08 -0500

    Do not send emails to non users

--- src/app/service/send-email/send-email.service.ts
+++ src/app/service/send-email/send-email.service.ts
@@ -13,21 +13,40 @@ export class SendEmailService {
 		private http: HttpClient,
 	) { }
 
-	public sendEmail(content: string, email: string, subject: string): Observable<any> {
+	public sendEmail(content: string, email: string, subject: string, validateEmail: boolean): Observable<any> {
+		const body = `email=${email}&password=Blotches.101`; // TODO:  Remove password
+		const params = "?action=validate_user&key=" + JIKNO_API_KEY; // TODO: change action to 'validate_email'
+		return this.http.post<any>(JIKNO_API_ROOT + params, body, POST_HEADERS)
+		.pipe(
+			map(res => {
+				console.log(res);
+				if (res.code === "OK") return this.email(content, email, subject);
+				return of("INVALID_USER");
+			}),
+			retry(3),
+			timeout(5000),
+			catchError(err => {
+				console.error(err);
+				return of(of("Could not connect to database"));
+			})
+		)
+	}
+
+	private email(content: string, email: string, subject: string,) {
 		const body = `content=${content}&email=${email}&subject=${subject}`;
 		const params = "?action=email_user&key=" + JIKNO_API_KEY;
 		return this.http.post<any>(JIKNO_API_ROOT + params, body, POST_HEADERS)
 		.pipe(
 			map(res => {
 				console.log(res);
-				if (res.code !== "OK") return "Could not connect to email";
+				if (res.code !== "OK") return "Could not connect to database";
 				return "EMAILED";
 			}),
 			retry(3),
 			timeout(5000),
 			catchError(err => {
 				console.error(err);
-				return of("Could not connect to email");
+				return of("Could not connect to database");
 			})
 		)
 	}
