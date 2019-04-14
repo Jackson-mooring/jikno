abc67fa2c8e736ef2101c574229a66bacc340b3c
Elisarmor <ejsisgreat@gmail.com>
2019-04-13 20:27:52 -0500

    Create service

--- src/app/service/apps-overlay/apps-overlay.service.ts
+++ src/app/service/apps-overlay/apps-overlay.service.ts
@@ -1,65 +1,57 @@
 import { Injectable } from '@angular/core';
 
 import { AppData } from '../../model/app-data';
+import { HttpClient } from '@angular/common/http';
+import { API_Response } from '../../model/api-response';
+import { JIKNO_API_ROOT, POST_HEADERS, JIKNO_API_KEY } from 'src/app/constants/constants';
+import { map, retry, timeout, catchError } from 'rxjs/operators';
+import { ValidationResponse } from '../../model/validationResponse';
+import { of } from 'rxjs';
+import { UserService } from '../user/user.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AppsOverlayService {
 
-	constructor() { }
+	error = '';
+	loading = false;
+	apps: AppData[] = []
 
-	apps: AppData[] = [
-		{
-			branch: "example_app",
-			route: '/example_app',
-			name: "Example App",
-			icon: "../../../../assets/logo-200.png",
-			active_by_user: true,
-			popularity: 0.5,
-			description: "This is an example app",
-			keyWords: [ 'example', 'app' ],
-		},
-		{
-			branch: "fake_software",
-			route: '/fake_software',
-			name: "Fake Software",
-			icon: "../../../../assets/logo-200.png",
-			active_by_user: true,
-			popularity: 0.5,
-			description: 'This is a fake software',
-			keyWords: [ 'fake', 'software', 'joke' ],
-		},
-		{
-			branch: "done",
-			route: '/done',
-			name: "Done",
-			icon: "../../../../assets/logo-200.png",
-			active_by_user: true,
-			popularity: 0.5,
-			description: 'This is a fake software',
-			keyWords: [ 'fake', 'software', 'joke' ],
-		},
-		{
-			branch: "counter",
-			route: '/counter',
-			name: "Counter",
-			icon: "../../../../assets/logo-200.png",
-			active_by_user: true,
-			popularity: 0.5,
-			description: 'This is a fake software',
-			keyWords: [ 'fake', 'software', 'joke' ],
-		},
-		{
-			branch: "search",
-			route: '/search',
-			name: "Search",
-			icon: "../../../../assets/logo-200.png",
-			active_by_user: true,
-			popularity: 0.5,
-			description: 'This is a fake software',
-			keyWords: [ 'fake', 'software', 'joke' ],
-		}
-	]
+	constructor(
+		private http: HttpClient,
+		private user: UserService,
+	) {
+		this.getApps();
+	}
+
+	getApps() {
+		const queryParams = `?key=${JIKNO_API_KEY}&action=get_content`;
+		const body = `email=${this.user.getUser().email}&password=${this.user.getUser().password}&branch_name=user_apps`;
+		this.http.post<API_Response>(JIKNO_API_ROOT + queryParams, body, POST_HEADERS)
+		.pipe(
+			retry(10),
+			timeout(5000),
+			catchError(err => {
+				console.error(err);
+				return of({correct: false, message: "Could not connect to database"})
+			}),
+			map((res: API_Response) => {
+				console.log(res);
+				if (res.code == "OK") return {
+					correct: true,
+					message: res.data
+				}; else return {
+					correct: false,
+					message: "Something went wrong."
+				}
+			})
+		)
+		.subscribe((data: ValidationResponse) => {
+			this.loading = false;
+			if (data.correct) this.apps = data.message;
+			else this.error = data.message;
+		})
+	}
 
 }
