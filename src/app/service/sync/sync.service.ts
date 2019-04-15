af9a422f65fa10203c96edc1496dcea4827fcb45
Elisarmor <ejsisgreat@gmail.com>
2019-04-15 16:56:39 -0500

    Finish up the alerts

--- src/app/service/sync/sync.service.ts
+++ src/app/service/sync/sync.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BRANCHES } from '../../constants/branches';
 import { SubDataService } from '../sub-data/sub-data.service';
+import { DataService } from '../data/data.service';
 
 @Injectable({
 	providedIn: 'root'
@@ -11,11 +12,14 @@ export class SyncService {
 
 	constructor(
 		private subData: SubDataService,
+		private dataService: DataService,
 	) {
-		this.sync();
+		this.sync(false);
 	}
 
-	sync() {
+	sync(sendStatus: boolean) {
+		if (sendStatus === undefined) sendStatus = true;
+
 		this.isSyncing = true;
 		var string = '';
 
@@ -24,19 +28,28 @@ export class SyncService {
 			if (branch != null) {
 				this.subData.subData(branch, i)
 				.subscribe(res => {
-					string += '#';
+					string += res ? '1' : '0';
 					if (string.length == BRANCHES.length) {
 						this.isSyncing = false;
-						alert("Everything up to date!");
+						if (sendStatus) this.check(string);
 					}
 				})
 			} else {
-				string += '#';
+				string += '1';
 				if (string.length == BRANCHES.length) {
 					this.isSyncing = false;
-					alert("Everything up to date!");
+					if (sendStatus) this.check(string);
 				}
 			}
+		}
+	}
+
+	check(str: string) {
+		if (str.indexOf('0') === -1) {
+			if (this.dataService.isInternet) alert("Everything up to date!");
+			else alert("Could not connect to database!");
+		}else{
+			alert("Could not connect to database!");
 		}
 	}
 
