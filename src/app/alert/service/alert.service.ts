f3757d3eec2df4fa1af33b8cf0e31fa3e9f0fefc
Elisarmor <ejsisgreat@gmail.com>
2019-04-16 19:22:06 -0500

    Create the values for the alert

--- src/app/alert/service/alert.service.ts
+++ src/app/alert/service/alert.service.ts
@@ -6,4 +6,28 @@ import { Injectable } from '@angular/core';
 export class AlertService {
 
 	constructor() { }
+
+	public showAlert: boolean = false;
+	public showCountdown: boolean = true;
+	public showTimes: boolean = false;
+
+	public countdownString: string;
+	public countdownTime: number = 5;
+
+	public expand: boolean = false;
+	public onclick: Function = () => { };
+	public onclose: Function = () => { };
+
+	public reset() {
+		this.showAlert = false;
+		this.showCountdown = true;
+		this.showTimes = false;
+
+		this.countdownString = undefined;
+		this.countdownTime = 5;
+
+		this.expand = false;
+		this.onclick = () => { };
+		this.onclose = () => { };
+	}
 }
