a254e746e5a7206742e5b056d13ea78e7b426e33
Elisarmor <ejsisgreat@gmail.com>
2019-04-19 18:57:46 -0500

    Create account component

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AccountComponent } from './components/account.component';
 
 @NgModule({
-	declarations: [],
+	declarations: [
+		AccountComponent
+	],
 	imports: [
 		CommonModule
 	]
