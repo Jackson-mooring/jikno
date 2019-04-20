f6b78249c722e071422b971eebf481ac2ded6e67
Elisarmor <ejsisgreat@gmail.com>
2019-04-20 16:55:34 -0500

    Create the content

--- src/app/service/account/account.service.ts
+++ src/app/service/account/account.service.ts
@@ -1,9 +1,71 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JIKNO_API_ROOT, POST_HEADERS, JIKNO_API_KEY } from '../../constants/constants';
+import { retry, map, catchError, timeout } from 'rxjs/operators';
+import { API_Response } from '../../model/api-response';
+import { ValidationResponse } from 'src/app/model/validationResponse';
+import { of } from 'rxjs';
+import { DataService } from '../data/data.service';
+import { UserService } from '../user/user.service';
+import { SubDataService } from '../sub-data/sub-data.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AccountService {
 
-	constructor() { }
+	constructor(
+		private http: HttpClient,
+		private dataService: DataService,
+		private userService: UserService,
+		private subDataService: SubDataService,
+	) {
+		this.getUserInfo();
+		setTimeout(() => {
+			this.getUserInfo();
+		}, 1000 * 60 * 10)
+	}
+
+	public userInfo: Object;
+	public loading = true;
+	public error: string;
+
+	getUserInfo() {
+		this.loading = true;
+
+		const params = `?action=get_user_info&key=${JIKNO_API_KEY}`;
+		const body = `password=${this.userService.getUser().password}&email=${this.userService.getUser().email}`;
+		this.http.post<API_Response>(JIKNO_API_ROOT + params, body, POST_HEADERS)
+		.pipe(
+			retry(3),
+			map(res => this.changeValues(res)),
+			catchError(err => {
+				console.error(err);
+				return of({correct: false, message: "Could not connect to database!"});
+			}),
+			timeout(5000)
+		)
+		.subscribe((res: ValidationResponse) => {
+			if (res.correct) {
+				this.userInfo = res.message;
+				this.dataService.isInternet = true;
+			}
+			else {
+				this.error = res.message;
+				this.dataService.isInternet = false;
+			}
+
+			this.loading = false;
+		})
+	}
+
+	private changeValues(res: API_Response): ValidationResponse {
+		if (res.code == "OK") return { correct: true, message: res.data };
+		else return { correct: false, message: res.data };
+	}
+
+	public setUserInfo() {
+		this.subDataService.subData(JSON.stringify(this.userInfo), 'user_info');
+	}
+
 }
