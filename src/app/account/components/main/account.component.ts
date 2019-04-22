faa2cec77fa31492cb10d8cf20ffdeec2bd20690
Elisarmor <ejsisgreat@gmail.com>
2019-04-22 15:08:50 -0500

    Prevent change the avatar

--- src/app/account/components/main/account.component.ts
+++ src/app/account/components/main/account.component.ts
@@ -4,7 +4,9 @@ import { AccountService } from 'src/app/service/account/account.service';
 import { TouchscreenService } from 'src/app/service/touchscreen/touchscreen.service';
 import { UploadFileService } from 'src/app/service/upload-file/upload-file.service';
 import { UserService } from '../../../service/user/user.service';
-import { last } from '@angular/router/src/utils/collection';
+import { JIKNO_API_ROOT } from '../../../constants/constants';
+import { SyncService } from '../../../service/sync/sync.service';
+import { RandomNumberService } from 'src/app/service/random-number/random-number.service';
 
 @Component({
 	selector: 'app-account',
@@ -19,6 +21,8 @@ export class AccountComponent implements OnInit {
 		public touchscreen: TouchscreenService,
 		private uploadFileService: UploadFileService,
 		private user: UserService,
+		private syncService: SyncService,
+		private randomNumberService: RandomNumberService,
 	) {
 		this.dataService.secondaryRoute = true;
 	}
@@ -36,12 +40,20 @@ export class AccountComponent implements OnInit {
 		
 		let image = (<HTMLInputElement>document.getElementById('mainAccountComponentFileChooser')).files[0]
 
+		let newName = this.changeName(image.name);
+
 		let file: FormData = new FormData();
-		file.append('file', image, this.changeName(image.name));
+		file.append('file', image, newName);
 
 		this.uploadFileService.upload(file)
 		.subscribe(res => {
-			console.log(res);
+			if (res.correct) {
+				this.accountService.userInfo.avatar = `${JIKNO_API_ROOT}cloud/avatars/${newName}`;
+				this.accountService.setUserInfo()
+				.subscribe(res => {
+					this.syncService.sync();
+				})
+			}
 		})
 	}
 
@@ -52,7 +64,7 @@ export class AccountComponent implements OnInit {
 			if (char === '.') lastDotIndex = index;
 		})
 
-		return `${this.user.getUser().email}${name.slice(lastDotIndex)}`
+		return `${this.user.getUser().email}${this.randomNumberService.getNumber(0, 90000)}${name.slice(lastDotIndex)}`
 	}
 
 }
