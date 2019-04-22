d373b16e9efb2a8535480f2a231b6ecb57319bb8
Elisarmor <ejsisgreat@gmail.com>
2019-04-22 15:54:14 -0500

    Create the danger template

--- src/app/account/components/danger/danger.component.ts
+++ src/app/account/components/danger/danger.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DangerComponent implements OnInit {
 
+	selectedOption = 'none';
+	showDanger = false;
+
 	constructor() { }
 
 	ngOnInit() {
