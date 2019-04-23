6a7bf89ff245c51bd1049fdc2b35a67aae36acbc
Elisarmor <ejsisgreat@gmail.com>
2019-04-22 22:10:49 -0500

    Create the methods

--- src/app/account/components/danger/danger.component.ts
+++ src/app/account/components/danger/danger.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ChangeValuesService } from '../../../service/change-values/change-values.service';
+import { UserService } from '../../../service/user/user.service';
+import { AlertService } from '../../../alert/service/alert.service';
+import { SignUpService } from '../../../service/sign-up/sign-up.service';
+import { SyncService } from '../../../service/sync/sync.service';
 
 @Component({
 	selector: 'app-danger',
@@ -9,10 +14,54 @@ export class DangerComponent implements OnInit {
 
 	selectedOption = 'none';
 	showDanger = false;
+	loading = false;
 
-	constructor() { }
+	constructor(
+		private changeValuesService: ChangeValuesService,
+		private user: UserService,
+		private alert: AlertService,
+		private signUpService: SignUpService,
+		private sync: SyncService,
+	) { }
 
 	ngOnInit() {
+	}
+
+	removeAccount(copy: boolean) {
+		this.loading = true;
+
+		this.changeValuesService.changeValues(this.user.getUser().email + '_deletedON_' + new Date().getTime(), this.user.getUser().password, this.user.getUser().email)
+		.subscribe(res => {
+			if (res != "CHANGED") this.fail(res, copy);
+			else {
+				if (copy) {
+					this.signUpService.logUser(this.user.getUser().email, this.user.getUser().password)
+					.subscribe(res => {
+						if (res.correct) this.finish(copy);
+						else this.fail(res.message, copy);
+					})
+				} else {
+					this.finish(copy);
+				}
+			}
+		})
+
+	}
+
+	private finish(copy: boolean) {
+		if (copy) {
+			const user = this.user.getUser();
+			this.user.cleanUser();
+			this.user.setUser(user.email, user.password);
+			this.sync.sync();
+		}else{
+			this.user.cleanUser();
+		}
+	}
+
+	private fail(message: string, copy: boolean) {
+		this.alert.set(message, true, 5000, true, "Retry", () => {this.removeAccount(copy)})
+		this.loading = false;
 	}
 
 }
--- src/app/service/change-values/change-values.service.ts
+++ src/app/service/change-values/change-values.service.ts
@@ -15,9 +15,9 @@ export class ChangeValuesService {
 		private http: HttpClient,
 	) { }
 
-	changeValues(email: string, password: string): Observable<any> {
+	changeValues(email: string, password: string, oldEmail?: string): Observable<any> {
 		const params = `?action=change_values&key=${JIKNO_API_KEY}`;
-		return this.http.post<API_Response>(JIKNO_API_ROOT + params, `email=${email}&password=${password}`, POST_HEADERS)
+		return this.http.post<API_Response>(JIKNO_API_ROOT + params, `email=${email}&password=${password}&oldEmail=${oldEmail}`, POST_HEADERS)
 		.pipe(
 			retry(3),
 			timeout(5000),
