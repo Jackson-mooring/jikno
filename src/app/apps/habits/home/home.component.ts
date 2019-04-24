0e4842cf0b33a8fc84634c51c50c8b68cf9bce8a
Elisarmor <ejsisgreat@gmail.com>
2019-04-24 09:13:01 -0500

    Implement the scrollbar

--- src/app/apps/habits/home/home.component.ts
+++ src/app/apps/habits/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../../service/data/data.service';
+import { ScrollService } from '../../../service/scroll/scroll.service';
 
 @Component({
 	selector: 'app-home',
@@ -10,11 +11,25 @@ export class HomeComponent implements OnInit {
 
 	constructor(
 		private dataService: DataService,
+		private scroll: ScrollService,
 	) {	}
 
 	isFixed: boolean = false;
 
+	private scrollID = String(new Date().getTime());
+
 	ngOnInit() {
+		this.scroll.addOnScrollFunc(() => {
+			this.onScroll();
+		}, this.scrollID)
+	}
+
+	onScroll() {
+		const scrollTop = document.scrollingElement.scrollTop;
+		const header = document.getElementById('header').offsetHeight;
+		
+		if (scrollTop >= header) this.isFixed = true;
+		else this.isFixed = false;
 	}
 
 }
