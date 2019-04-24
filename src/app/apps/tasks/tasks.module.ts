bf56634e9460d55c76951dded11529bccfa03e4e
Jackson <jackson.mooring@gmail.com>
2019-04-24 15:23:18 -0500

    Create Tasks module

---
+++ src/app/apps/tasks/tasks.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksHomeComponent } from './home/home.component';
+
+
+@NgModule({
+	declarations: [
+		TasksHomeComponent
+	],
+	imports: [
+		CommonModule
+	]
+})
+export class TasksModule { }
