721b272777c426fa4411d684d77140ef686007f7
Elisarmor <ejsisgreat@gmail.com>
2019-04-24 12:19:36 -0500

    Create the basic components

--- src/app/apps/habits/habits.module.ts
+++ src/app/apps/habits/habits.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home/home.component';
 import { CoreModule } from '../../shared/core.module';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReorderComponent } from './reorder/reorder.component';
+import { NewHabitComponent } from './new-habit/new-task.component';
 
 @NgModule({
 	declarations: [
-		HomeComponent
+		HomeComponent,
+		DashboardComponent,
+		ReorderComponent,
+		NewHabitComponent,
 	],
 	imports: [
 		CommonModule,
 		CoreModule,
+		RouterModule,
 	]
 })
 export class HabitsModule { }
--- src/app/apps/habits/routes/routes.ts
+++ src/app/apps/habits/routes/routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from '../home/home.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ReorderComponent } from '../reorder/reorder.component';
+import { NewHabitComponent } from '../new-habit/new-task.component';
 
 export const HabitsRoutes: Routes = [
-	{ path: 'habits', component: HomeComponent }
+	{ path: 'habits', component: HomeComponent },
+	{ path: 'habits/dashboard', component: DashboardComponent },
+	{ path: 'habits/reorder', component: ReorderComponent },
+	{ path: 'habits/new-habit', component: NewHabitComponent },
 ]
