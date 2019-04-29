aa09e52f03c5aa662e2d11f902685ab034cdecc9
Jackson <jackson.mooring@gmail.com>
2019-04-29 11:03:57 -0500

    Get test info out

--- src/app/apps/tasks/home/home.component.ts
+++ src/app/apps/tasks/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { StickScrollBarComponent } from '../../../stick-scroll-bar/stick-scroll-bar.component'
+import { DataService } from 'src/app/service/data/data.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
 export class TasksHomeComponent implements OnInit {
+	public isFixed = false
+	constructor(
+		public sharedModule: SharedModule,
+		public dataService: DataService,
+	) { dataService.showBottomSpace = false;
+		dataService.secondaryRoute = false; 
+	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
+	onScroll(top: number, header: HTMLElement) {
+		if (top >= header.offsetHeight + 42.88) this.isFixed = true;
+		else this.isFixed = false;
+	}
 
 }
